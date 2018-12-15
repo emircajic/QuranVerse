bfe3be3d3ed464fdfef231c59300199df4685de5
Emir Cajic <e.cajic@gmail.com>
2018-12-15 22:26:34 +0100

    Activation script finished
    Activation script checks if there is table present in wpdb, and if there is no table present, it creates one and populates it with Quranic uthmani text.

--- includes/class-quranverse-activator.php
+++ includes/class-quranverse-activator.php
@@ -30,7 +30,38 @@ class Quranverse_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		global $wpdb;
+		$table_name = $wpdb->prefix.'quran';
+		if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+			//table not in database. Create new table
+			$charset_collate = $wpdb->get_charset_collate();
 
+			$sql = "CREATE TABLE $table_name (
+					  `index` int(4),
+					  `sura` int(3),
+					  `aya` int(3),
+					  `text` text,
+					  PRIMARY KEY  (`index`)
+					) $charset_collate";
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+
+			$row = 1;
+			if (($handle = fopen(__DIR__."/sources/quran-uthmani.txt", "r")) !== FALSE) {
+				while ($row<=6236) {
+					$data = fgetcsv($handle, 0, "|");
+					$data = array('index' => $row, 'sura' => $data[0], 'aya' => $data[1], 'text' => $data[2]);
+					$format = array('%d','%d','%d','%s');
+					$wpdb->insert($table_name, $data, $format);
+					$row++;
+				}
+				fclose($handle);
+			}
+
+		}
+		if (!get_option('quranverse')) {
+			add_option('quranverse_needs_setup',true);
+		}
 	}
 
 }
